a04e567237f297d5616a134a16eba576520f8bbf
Tang Haozhe <tanghaozhe@outlook.com>
2022-03-19 15:12:35 +0900

    refactor: Refactor classes

--- modules.py
+++ modules.py
@@ -1,24 +1,46 @@
-class Wand:
+from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
+import cv2
+import numpy as np
+
+class Observer(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self) -> None:
+        pass
+    def update(self):
+        pass
+
+class WandObserver(Observer):
     def __init__(self, name, template, color):
         self.name = name
         self.template = template
         self.pointer = -1
-        self.detect_flag = False
-        self.points = []
+        self.is_detected = False
+        self.points = [[]]
         self.signals = []
         self.color = color
         self.diff_frames = 0
+        self.stroke_ix = 0
+        self.pre_signal = 0
 
+    def __call__(self,signal):
+        self.signals.append(signal)
+        self.pre_signal = signal
+        if not self.is_detected:
+            self.check(signal)
+        else:
+            self.update(signal)
 
     def check(self, signal):
+        # matching signals with template pattern
         if self.template[self.pointer+1] == signal:
             self.pointer += 1
         else:
             self.pointer = -1
         if self.pointer == len(self.template)-1:
-            self.detect_flag = True
+            self.is_detected = True
             self.pointer = 0
-            print("wand detectd!")
+            print(f"{self.name} detectd!")
 
     def update(self,signal):
         if self.template[self.pointer] != signal:
@@ -30,36 +52,20 @@ class Wand:
             self.pointer = 0
 
     def abort(self):
+        self.stroke_ix += 1
         self.pointer = -1
-        self.detect_flag = False
-        self.points = []
+        self.is_detected = False
+        self.points.append([])
         self.signals = []
         self.diff_frames = 0
-        print("aborted!")
+        self.pre_signal = 0
+        print(f"{self.name} aborted!")
 
-
-# wand1 = Wand([0,1,1])
-# records = []
-# signals = []
-# while 1:
-#     points = input("input:")
-#     if points:
-#         points = [int(x) for x in points.split(" ")]
-#         records.append([points])
-#         signal = 1
-#     else:
-#         # print("nothing detected!")
-#         records.append([])
-#         signal = 0
-#     signals.append(signal)
-    
-#     if not wand1.detect_flag:
-#         wand1.check(signal)
-#     else:
-#         wand1.update(signal)
-    # print("pointer:",wand1.pointer)
-    
-    
+    def draw(self, frame):
+        for pts in self.points:
+            pts = np.array(pts, dtype=np.int32)
+            cv2.polylines(frame,[pts],False,self.color,3)
+        
 
     
     
--- track.py
+++ track.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import imutils
 from imutils import contours
-from modules import Wand
+from modules import WandObserver
 import math
 
 def get_points(frame):
@@ -13,8 +13,6 @@ def get_points(frame):
     thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.erode(thresh, None, iterations=2)
     thresh = cv2.dilate(thresh, None, iterations=4)
-    # cv2.imwrite(filename+".jpg", frame)
-    
 
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
@@ -36,24 +34,16 @@ def get_points(frame):
             # cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
     return good_pt
 
-def get_signal_n(diff_frames):
+def get_signal_num(diff_frames):
     if diff_frames >= 13:
         wand_signal_n = 2
     elif diff_frames >= 0 and diff_frames <= 13:
         wand_signal_n = 1
     return wand_signal_n
 
-def do_wand(wand,signal):
-    wand.signals.append(signal)
-    
-    if not wand.detect_flag:
-        wand.check(signal)
-    else:
-        wand.update(signal)
-
 def main():
-    wand1 = Wand("wand1",[1,0,1,0],(0,0,255))
-    wand2 = Wand("wand2",[1,1,0,0],(0,255,0))
+    wand1 = WandObserver("wand1",[1,0,1,0],(0,0,255))
+    wand2 = WandObserver("wand2",[1,1,0,0],(0,255,0))
     wands = [wand1,wand2]
     videofile_path = "./data/0.5.mp4"
     cap = cv2.VideoCapture(videofile_path)
@@ -67,66 +57,59 @@ def main():
 
     rval, frame = cap.read()
     count = 0
-    diff_frames = 0
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     videowriter = cv2.VideoWriter("./output/output.mp4",fourcc, 30, (width,height))
     while 1:
         rval, frame = cap.read()
         if not rval:
             break
-        
         # filename = os.path.sep.join(["./output", "test_{}".format(count)])
         count += 1
-        diff_frames += 1
         points = get_points(frame)
-        
 
         for wand in wands:
             wand.diff_frames += 1
-            run_wand_flag = False
+            is_signal_changed = False
             cur_signal = 0
+            stroke_ix = wand.stroke_ix
             if len(points) > 0:
-                if len(wand.points) > 0:
-                    pre_point = wand.points[-1]
+                if len(wand.points[stroke_ix]) > 0:
+                    pre_point = wand.points[stroke_ix][-1]
                     dists = [math.sqrt((pre_point[0]-s0)**2 + (pre_point[1]-s1)**2) for s0, s1 in points]
                     min_dist = min(dists)
                     if min_dist < 100:
                         cur_signal = 1
-                        neigh_point = points[dists.index(min_dist)]
+                        nt_point = points[dists.index(min_dist)]
                     else:
                         wand.abort()
                 else:
                     cur_signal = 1
-                    neigh_point = points[0]  # FIXME     
+                    nt_point = points[0]  # FIXME     
 
-            if len(wand.signals)==0:
-                pre_signal = 0
-            else:
-                pre_signal = wand.signals[-1]
+            pre_signal = wand.pre_signal
 
             if pre_signal==0 and cur_signal==1:
-                run_wand_flag = True
+                is_signal_changed = True
                 wand_signal = 1
-                wand_signal_n = get_signal_n(wand.diff_frames)
+                wand_signal_n = get_signal_num(wand.diff_frames)
                 # print("light frames:",wand.diff_frames,"count:",count)
                 wand.diff_frames = 0
 
             elif pre_signal==1 and cur_signal==0:
-                run_wand_flag = True
+                is_signal_changed = True
                 wand_signal = 0
                 # print("black frames:",wand.diff_frames,"count:",count)
-                wand_signal_n = get_signal_n(wand.diff_frames)
+                wand_signal_n = get_signal_num(wand.diff_frames)
                 wand.diff_frames = 0
 
-            if run_wand_flag:
+            if is_signal_changed:
                 for i in range(wand_signal_n):
-                    do_wand(wand,wand_signal)
+                    wand(wand_signal)
 
-            if wand.detect_flag:
-                wand.points.append(neigh_point)
-
-            pts = np.array(wand.points, dtype=np.int32)
-            cv2.polylines(frame,[pts],False,wand.color,3)
+            if wand.is_detected:
+                wand.points[stroke_ix].append(nt_point)
+            
+            wand.draw(frame)
         videowriter.write(frame)
         
     videowriter.release()
